ff2072a860ce6638b7b211014348db0dc8928752
robfil50219 <robfil50219@stud.noroff.no>
2024-09-19 15:41:05 +0200

    styles changed

--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -1,11 +1,17 @@
 // cypress/e2e/login_spec.cy.js
 describe('Login Test', () => {
     it('logs in with valid credentials', () => {
+      // Visit the login page
       cy.visit('https://auctionarygo.netlify.app/login.html');
-      cy.get('input[name="email"]').type('robfil50219@stud.noroff.no');
-      cy.get('input[name="password"]').type('Halflife');
+
+      // Fill in the email and password fields
+      cy.get('#email').type('robfil50219@stud.noroff.no');
+      cy.get('#password').type('Halflife');
+
+      // Submit the login form
       cy.get('button[type="submit"]').click();
-      cy.url().should('include', '/dashboard'); // Verify redirection after login
+
+      // Check for successful login by confirming if the Logout button appears
+      cy.get('#logout-btn').should('be.visible');
     });
-  });
-  
+});
